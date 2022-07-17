ca46d61dba67bd805f3447e4115a87e0e90d9253
S. M. Zuhair Zaki <1705023@ugrad.cse.buet.ac.bd>
2022-07-18 02:57:09 +0600

    Add files via upload

--- pipeline.cpp
+++ pipeline.cpp
@@ -2,55 +2,14 @@
 #include<iostream>
 #include<fstream>
 #include<cmath>
+#include<cstdlib>
+#include<ctime>
 #include<stack>
+#include "bitmap_image.hpp"
 
 #define pi (2*acos(0.0))
 
 using namespace std;
-
-class point
-{
-public:
-    double x, y, z, w;
-
-    point();
-    point(double,double,double);
-    point(double,double,double,double);
-    void make_homogeneous();
-};
-
-point::point()
-{
-        x = 0;
-        y = 0;
-        z = 0;
-        w = 1;
-}
-
-point::point(double px, double py, double pz)
-{
-        x = px;
-        y = py;
-        z = pz;
-        w = 1;
-}
-
-point::point(double px, double py, double pz, double pw)
-{   
-    x = px;
-    y = py;
-    z = pz;
-    w = pw;
-}
-
-void point::make_homogeneous()
-{
-    x /= w;
-    y /= w;
-    z /= w;
-    w = 1;
-}
-
 
 class Vector
 {
@@ -138,6 +97,55 @@ Vector operator*(Vector const& vec, double scale)
     res.y = vec.y*scale;
     res.z = vec.z*scale;
     return res;
+}
+
+class point
+{
+public:
+    double x, y, z, w;
+
+    point();
+    point(double,double,double);
+    point(double,double,double,double);
+    void make_homogeneous();
+    Vector toVector();
+};
+
+point::point()
+{
+        x = 0;
+        y = 0;
+        z = 0;
+        w = 1;
+}
+
+point::point(double px, double py, double pz)
+{
+        x = px;
+        y = py;
+        z = pz;
+        w = 1;
+}
+
+point::point(double px, double py, double pz, double pw)
+{   
+    x = px;
+    y = py;
+    z = pz;
+    w = pw;
+}
+
+void point::make_homogeneous()
+{
+    x /= w;
+    y /= w;
+    z /= w;
+    w = 1;
+}
+
+Vector point::toVector()
+{
+    return Vector(x,y,z);
 }
 
 class Matrix
@@ -351,8 +359,121 @@ Matrix createScalingMatrix(double sx, double sy, double sz)
     return S;
 }
 
+struct Color
+{
+    double r,g,b;
+};
+
+struct  Triangle{
+    point Points[3];
+    Color color;
+};
+
 Vector eye, look, up;
 double fovY, aspect, zNear, zFar;
+
+double screen_width, screen_height;
+double x_lim, y_lim;
+double zFront, zRear;
+
+double top_scanline(Triangle tri, double top_y, double del_y)
+{
+    double max_y = tri.Points[0].y;
+    for(int i=1;i<3;i++)
+    {
+        if(tri.Points[i].y > max_y)
+            max_y = tri.Points[i].y;
+    }
+
+    for(int i=0;i<screen_height;i++)
+    {
+        double cell_y = top_y - i*del_y;
+        if(max_y > cell_y)
+            return i;
+    }
+
+    return screen_height;
+}
+
+double bottom_scanline(Triangle tri, double bottom_y, double del_y)
+{
+    double min_y = tri.Points[0].y;
+    for(int i=1;i<3;i++)
+    {
+        if(tri.Points[i].y < min_y)
+            min_y = tri.Points[i].y;
+    }
+
+    for(int i=0;i<screen_height;i++)
+    {
+        double cell_y = bottom_y + i*del_y;
+        if(min_y < cell_y)
+            return screen_height - i - 1;
+    }
+
+    return -1;
+}
+
+double* left_right_scanline(Triangle tri, double row, double left_x, double right_x, double del_x)
+{
+    Vector v1 = tri.Points[1].toVector() - tri.Points[0].toVector();
+    Vector v2 = tri.Points[2].toVector() - tri.Points[1].toVector();
+    Vector v3 = tri.Points[0].toVector() - tri.Points[2].toVector();
+
+    double t1 = (row - v1.y)/tri.Points[0].y;
+    double t2 = (row - v2.y)/tri.Points[1].y;
+    double t3 = (row - v3.y)/tri.Points[2].y;
+
+    double max_x = left_x -1, min_x = right_x + 1;
+    if(t1 > 0 && t1 < 1)
+    {
+        double x = tri.Points[0].x + t1*v1.x;
+        if(x > max_x)
+            max_x = x;
+        if(x < min_x)
+            min_x = x;
+    }
+    if(t2 > 0 && t2 < 1)
+    {
+        double x = tri.Points[1].x + t2*v2.x;
+        if(x > max_x)
+            max_x = x;
+        if(x < min_x)
+            min_x = x;
+    }
+    if(t3 > 0 && t3 < 1)
+    {
+        double x = tri.Points[2].x + t3*v3.x;
+        if(x > max_x)
+            max_x = x;
+        if(x < min_x)
+            min_x = x;
+    }
+
+    double* res = new double[2];
+    res[0] = screen_width;
+    res[1] = -1;
+
+    for(int i=0;i<screen_width;i++)
+    {
+        double cell_x = left_x + i*del_x;
+        if(min_x < cell_x){
+            res[0] = i;
+            break;
+        }
+    }
+
+    for(int i=0;i<screen_width;i++)
+    {
+        double cell_x = right_x - i*del_x;
+        if(max_x > cell_x){
+            res[0] = screen_width - i - 1;
+            break;
+        }
+    }
+
+    return res;
+}
 
 void stage1()
 {
@@ -492,8 +613,139 @@ void stage3()
     fout.close();
 }
 
+void stage4()
+{   
+    ifstream conf("config.txt");
+
+    conf >> screen_width >> screen_height;
+    conf >> x_lim;
+    conf >> y_lim;
+    conf >> zFront >> zRear;
+
+    conf.close();
+
+    ifstream fin("stage3.txt");
+    ofstream fout("stage4.txt");
+    ofstream zout("z_buffer.txt");
+    
+    double** zBuffer = (double**) malloc(screen_height*sizeof(double*));
+    for(int i=0;i<screen_height;i++)
+        zBuffer[i] = (double*) malloc(screen_width*sizeof(double));
+
+    for(int i=0;i<screen_height;i++)
+        for(int j=0;j<screen_width;j++)
+            zBuffer[j][i] = zRear;    
+
+    bitmap_image image(screen_width,screen_height);
+    for(int i=0;i<screen_height;i++)
+        for(int j=0;j<screen_width;j++)
+            image.set_pixel(i,j,255,255,255);
+
+    double dx = (-x_lim*2)/screen_width;
+    double dy = (-y_lim*2)/screen_height;
+    double Top_y = -y_lim - dy/2;
+    double Left_x = x_lim + dx/2;
+
+    while(!fin.eof())
+    {
+        Triangle t;
+        for(int i=0;i<3;i++)
+        {   
+            double x,y,z;
+            fin >> x >> y >> z;
+            
+            if(fin.eof())
+                break;
+            
+            point p(x,y,z);
+            t.Points[i] = p;
+        }
+
+        if(fin.eof())
+            break;
+
+        t.color.r = rand()%256;
+        t.color.g = rand()%256;
+        t.color.b = rand()%256;
+
+        Vector n = (t.Points[1].toVector() - t.Points[0].toVector()).cross(t.Points[2].toVector() - t.Points[0].toVector());
+        n.normalize();
+        double d = n.dot(t.Points[0].toVector());
+
+        double top_row = top_scanline(t,Top_y,dy);
+        double bottom_row = bottom_scanline(t,-Top_y,dy);
+        for(int i=top_row;i<=bottom_row;i++)
+        {   
+            double cell_y = Top_y - i*dy;
+            double* cols = left_right_scanline(t,i,Left_x,-Left_x,dx);
+
+            for(int j=cols[0];j<=cols[1];j++)
+            {   
+                double cell_x = Left_x + j*dx;
+                double cell_z = (d - n.x*cell_x - n.y*cell_y)/n.z;
+                
+                if(cell_z > zFront)
+                {
+                    if(cell_z < zBuffer[j][i])
+                    {
+                        zBuffer[j][i] = cell_z;
+                        image.set_pixel(j,i,t.color.r,t.color.g,t.color.b);
+                    }
+                }
+            }
+
+            delete[] cols;
+        }
+    }
+
+    for(int i=0;i<screen_height;i++)
+    {
+        for(int j=0;j<screen_width;j++)
+            zout << zBuffer[i][j] << " ";
+        zout<<endl;
+    }
+
+    image.save_image("output.bmp");
+
+    for(int i=0;i<screen_height;i++)
+        delete[] zBuffer[i];
+    delete zBuffer;
+
+    fin.close();
+    fout.close();
+    zout.close();
+}
+
 int main()
 {   
+    srand(time(NULL));
+
+    ifstream fin("test.txt");
+
+    while(!fin.eof())
+    {
+        Triangle t;
+        for(int i=0;i<3;i++)
+        {   
+            double x,y,z;
+            fin >> x >> y >> z;
+
+            if(fin.eof())
+                break;
+
+            cout << x << " " << y << " " << z << endl;
+            
+            point p(x,y,z);
+            t.Points[i] = p;
+        }
+
+        if(fin.eof())
+            break;
+
+        cout<<"Triangle"<<endl;
+    }
+
+    fin.close();
     return 0;
 }
 
